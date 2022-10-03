251d7ca60aac7a30155a87b2a3f559372a5b8c04
Karlsson Per-Erik <per-erik.karlsson@tieto.com>
2022-10-03 10:37:21 +0200

    Update scrape for 365 days.

--- process-nordpol.js
+++ process-nordpol.js
@@ -1,7 +1,28 @@
 const axios = require('axios');
 const fs = require('fs');
+const luxon = require('luxon');
 
-axios.get('https://www.nordpoolgroup.com/api/marketdata/chart/29?currency=,,,EUR')
-  .then(r => r.data.data.Rows.map(row => ({ t: row.StartTime, y: Number(row.Columns[3].Value.replace(',','.')) })))
-  .then(r => fs.writeFileSync('data.json', JSON.stringify(r, { indent: 2 })))
+function uniq(a, key) {
+  var seen = {};
+  return a.filter(function(item) {
+      return seen.hasOwnProperty(item[key]) ? false : (seen[item[key]] = true);
+  });
+}
+
+data$ = Array(365).fill().map((_, i) => {
+  const endTime = luxon.DateTime.now().minus({ days: i }).toFormat('dd-MM-yyyy');
+
+  return new Promise(resolve => setTimeout(resolve, 1000 * i))
+    .then(() => axios.get(`https://www.nordpoolgroup.com/api/marketdata/page/29?currency=,,,EUR&endDate=${endTime}`))
+    .then(r => r.data.data.Rows.map(row => ({ t: row.StartTime, y: Number(row.Columns[3].Value.replace(',','.')) })))
+})
+
+data$.forEach((r, i) => r.then(() => console.log(i)));
+
+Promise.all(data$).then(response => {
+  const flat = response.flat();
+  const unique = uniq(flat, 't')
+
+  fs.writeFileSync('data.json', JSON.stringify(unique, null, 2));
+})
   
